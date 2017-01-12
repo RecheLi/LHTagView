afbe576d051053a3f7b600bec5cc4b5fad70c937
Linitial <linitial@qq.com>
2017-01-12 16:45:17 +0800

    首次上传

---
+++ LHTagView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHTagView
+//
+//  Created by Apple on 17/1/12.
+//  Copyright © 2017年 Linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LHTagView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LHTagView
+//
+//  Created by Apple on 17/1/12.
+//  Copyright © 2017年 Linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

9d4fa4cc819f6dbc2e49f4ce0cddad54f8120d53
Linitial <linitial@qq.com>
2017-01-13 14:54:39 +0800

    更新展示图片

--- LHTagView/ViewController.m
+++ LHTagView/ViewController.m
@@ -26,41 +26,48 @@
 
 - (void)getData {
     LHTagModel *model1 = [[LHTagModel alloc]init];
-    model1.name = @" name one ";
-    model1.detail = @" detail one ";
+    model1.name = @" angelababy ";
+    model1.detail = @" xiaoming ";
     model1.Left = 30.0;
-    model1.Top = 25.6;
+    model1.Top = 75.6;
     model1.Direction = 3;
     LHTagView *view1 = [[LHTagView alloc]initWithTagModel:model1 superview:_imageView];
     view1.modelBlock = ^(LHTagModel *model){
-        NSLog(@"model1.left %f",model.Left);
     };
     view1.dotColor = [UIColor cyanColor];
     view1.nameColor = [UIColor purpleColor];
     
     LHTagModel *model2 = [[LHTagModel alloc]init];
-    model2.name = @" 我的自白 ";
-    model2.detail = @" 😄 ";
-    model2.Left = 140.0;
+    model2.name = @" bangbangbang ";
+    model2.detail = @" 😄 baby baby baby 😊 ";
+    model2.Left = 65.0;
     model2.Top = 259.6;
     model2.Direction = 5;
     LHTagView *view2 = [[LHTagView alloc]initWithTagModel:model2 superview:_imageView];
     view2.modelBlock = ^(LHTagModel *model){
-        NSLog(@"model2.left %f",model.Left);
     };
     view2.bgColor = [UIColor greenColor];
     
     LHTagModel *model3 = [[LHTagModel alloc]init];
-    model3.name = @" 年会没中奖 ";
+    model3.name = @"年会没中奖😢";
     model3.detail = @"呵呵哒";
-    model3.Left = 60.0;
-    model3.Top = 112.6;
+    model3.Left = 107.0;
+    model3.Top = 154.6;
     model3.Direction = 4;
     LHTagView *view3 = [[LHTagView alloc]initWithTagModel:model3 superview:_imageView];
     view3.modelBlock = ^(LHTagModel *model){
-        NSLog(@"model3.left %f",model.Left);
     };
     view3.nameColor = [UIColor redColor];
+    
+    LHTagModel *model4 = [[LHTagModel alloc]init];
+    model4.name = @"靓女😍";
+    model4.detail = @"😍";
+    model4.Left = 107.0;
+    model4.Top = 334.6;
+    model4.Direction = 6;
+    LHTagView *view4 = [[LHTagView alloc]initWithTagModel:model4 superview:_imageView];
+    view4.modelBlock = ^(LHTagModel *model){
+    };
 
 }
 
